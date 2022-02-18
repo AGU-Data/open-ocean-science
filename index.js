1c00cbb50516aee95e1fa9902b0ed16097f05658
James Munroe <jmunroe@mun.ca>
2022-02-18 15:06:44 -0500

    Add a LeafletJS map of the world to the web site

--- index.js
+++ index.js
@@ -19,3 +19,11 @@ fetch('people.json')
     .catch(function (err) {
         console.log('error: ' + err);
     });
+
+
+/* map */
+var map = L.map('mapid').setView([0, 0], 2);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
